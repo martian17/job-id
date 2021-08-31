2eb092878fd0e003d9a890a7d1c75f64782e99d8
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-09-01 02:36:40 +0900

    setup all files

---
+++ job-id.js
@@ -0,0 +1,35 @@
+//util
+let isObjEmpty = function(obj){
+    for(let i in obj){
+        return false;
+        break;
+    }
+    return true;
+};
+
+let ID = (new function(){
+    let free = {};
+    let used = {};
+    let max = 0;
+    this.get = function(){
+        if(isObjEmpty(free)){
+            free[max] = 1;
+            max++;
+        }
+        for(let id in free){
+            delete free[id];
+            used[id] = 1;
+            return id;
+        }
+    };
+    this.free = function(id){
+        if(id in used){
+            delete used[id];
+            free[id] = 1;
+        }else{
+            console.log(`warning: the id given is not used (${id})`);
+        }
+    };
+}());
+
+
